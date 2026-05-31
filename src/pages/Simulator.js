ef4355b64ebd6d481a78fc2bc7e245b0305324ae
SSaha <ssaha@duck.com>
2026-05-31 16:47:54 -0500

    remove usereffect

--- src/pages/Simulator.js
+++ src/pages/Simulator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { PageHeader, Footer } from '../components/shared';
 
 const simulatorConfigs = {
@@ -24,7 +24,7 @@ function calculateImpacts(state) {
 }
 
 function impactNarratives(state) {
-  //const { category, magnitude, select, targets } = state;
+  const { category, magnitude, select, targets } = state;
   const targetText = targets.join(', ');
   const selected = select.toLowerCase();
   if (category==='TARIFFS') return [
